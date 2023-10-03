94591e0b9a6467699bc70c5200091ca34575f228
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2023-10-03 19:43:31 +0200

    - Added footer. - Added Partners section. - Added CSS for both missing sections.

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import '@fortawesome/fontawesome-svg-core/styles.css';
 import { Description } from '@/components/Pages/Description/Description';
 import { Header } from '@/components/Layout/Header/Header';
 import { Marquee } from '@/components/Layout/Marquee/Marquee';
 import { Information } from '@/components/Pages/Information/Information';
 import { Contact } from '@/components/Pages/Contact/Contact';
 import { Crypto } from '@/components/Pages/Crypto/Crypto';
+import { Partners } from '@/components/Pages/Partners/Partners';
+import { Footer } from '@/components/Layout/Footer/Footer';
 
 export default function Home() {
     return (
@@ -15,6 +18,8 @@ export default function Home() {
             <Information />
             <Contact />
             <Crypto />
+            <Partners />
+            <Footer />
         </div>
     );
 }
